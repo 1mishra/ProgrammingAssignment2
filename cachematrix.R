3ed72ab22a413a663f3c6cfe60d39ea9efb38215
shivank.mishra@gmail.com <shivank.mishra@gmail.com>
2016-05-22 08:49:23 +0530

    edit ccachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Matrix inversion is usually a costly computation and there may be some benefit to
+##caching the inverse of a matrix rather than compute it repeatedly (there are also alternatives to matrix inversion 
+##that we will not discuss here). Your assignment is to write a pair of functions that cache the inverse of a matrix.
 
-## Write a short comment describing this function
+#makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        #set the value of the matrix
+        
+        I <- NULL
+        set <- function(y) {
+                x <<- y
+                I <<- NULL
+        }
+        
+        #get the value of the matrix
+        get <- function() x
+        
+        #set the value of the inverse 
+        setinverse <- function(inverse) I <<- inverse
+        #get the value of the inverse
+        getinverse <- function() I
+        list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+#cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+#If the inverse has already been calculated (and the matrix has not changed), 
+#then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        I <- x$getinverse()
+        
+        # retrieves the inverse matrix x, if it has been calculated before
+        if(!is.null(I)) {
+                message("retrieving cached data")
+                return(I)
+        }
+        # otherwise, solves the matrix
+        data <- x$get()
+        I <-  solve(data, ...)
+        x$setinverse(I)
+        I
 }
